295546e658862e28146ad8ebbaeb539a31a748f2
tylerwatson98 <32936743+tylerwatson98@users.noreply.github.com>
2018-10-02 13:01:42 -0600

    Add license to top of main activity

--- app/src/main/java/com/example/tpwatson_feelsbook/MainActivity.java
+++ app/src/main/java/com/example/tpwatson_feelsbook/MainActivity.java
@@ -1,7 +1,30 @@
 /*
-tpwatson-feelsbook: Store emotion entries created by the user
+MIT License
 
+Copyright (c) 2018 Tyler Watson
 
+Program: tpwatson-FeelsBook
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+
+/*
     The MainActivity will contain all the emotion buttons and the browse activity button. On the click of an emotion
     button the user is taken to the EmotionEntry activity where they can submit the emotion with an optional comment. The
     browse emotions button at the bottom of the screen will bring the user to the screen containing all the stored emotions.
